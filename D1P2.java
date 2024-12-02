6fbf2b10fa204ab6bf42a281a27decac16b5c0ee
wshin <wl751242@dal.ca>
2024-12-02 18:54:55 -0400

    attempt at day 1 part 2 in java

--- D1P2.java
+++ D1P2.java
@@ -1,5 +1,49 @@
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+import java.io.File;
+import java.util.Map;
+import java.util.HashMap;
+import java.util.List;
+import java.util.ArrayList;
+import java.io.FileNotFoundException;
+
 public class D1P2{
   public static void main(String[] args){
+    Scanner in = new Scanner(System.in);
+    System.out.print("Enter file name: ");
+    String fileName = in.nextLine();
+
+    try{
+      File file = new File(fileName);
+      Scanner fileReader = new Scanner(file);
+      List<Integer> left = new ArrayList<>();
+      List<Integer> right = new ArrayList<>();
+
+      while (fileReader.hasNextLine()){
+        left.add(fileReader.nextInt());
+        right.add(fileReader.nextInt());
+      }
+
+      Map<Integer, Integer> frequencyByLocation = new HashMap<>();
+      for (Integer num : right){
+        if (!frequencyByLocation.containsKey(num)){
+          frequencyByLocation.putIfAbsent(num, 1);
+        }
+        else{
+          frequencyByLocation.computeIfPresent(num, (key, val) -> val + 1);
+        }
+      }
+
+      int totalSimilarityScore = 0;
+      for (Integer location : left){
+        totalSimilarityScore += (location * frequencyByLocation.getOrDefault(location, 0));
+      }
+
+      System.out.println("Total similarity score: " + totalSimilarityScore);
+    }
+    catch (FileNotFoundException e){
+      System.out.println("File not found!");
+    }
 
   }
 }
